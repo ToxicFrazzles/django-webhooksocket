4301bce22b726690a85bbe503462031f4d169363
ToxicFrazzles <frazzles740@gmail.com>
2021-06-02 21:07:47 +0100

    Baseline project
    Setup works.
    Development environment set up

---
+++ boot_django.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import django
+from django.conf import settings
+
+BASE_DIR = Path(__file__).parent / "webhooksocket"
+
+
+def boot_django():
+    settings.configure(
+        BASE_DIR=BASE_DIR,
+        DEBUG=True,
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME":   BASE_DIR / "db.sqlite3",
+            }
+        },
+        INSTALLED_APPS=(
+            "receipts",
+        ),
+        TIME_ZONE="UTC",
+        USE_TZ=True,
+    )
+    django.setup()
